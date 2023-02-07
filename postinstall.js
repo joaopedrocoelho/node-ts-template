ea75fe1f5afbfcc67bd20eb0a148ca236a895f41
Pedro <pedro@webglsoft.com>
2023-02-07 13:25:02 +0800

    :sparkles: initial commit

---
+++ postinstall.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const exec = require('child_process').exec;
+
+exec('npm install', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
