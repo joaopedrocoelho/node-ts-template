34dfe480d40025396f1c40c3d220c061b3bad53e
Pedro <pedro@webglsoft.com>
2023-02-07 13:30:45 +0800

    :sparkles: changed postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 const exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
+
+const templatePath = __dirname;
+const targetPath = path.resolve(process.cwd(), process.argv[2]);
+
+fs.mkdirSync(targetPath, { recursive: true });
+
+const copyFile = (file) => {
+  const templateFile = path.resolve(templatePath, file);
+  const targetFile = path.resolve(targetPath, file);
+
+  const content = fs.readFileSync(templateFile, 'utf-8');
+  fs.writeFileSync(targetFile, content);
+};
+
+fs.readdirSync(templatePath).forEach(copyFile);
 
 exec('npm install', (error, stdout, stderr) => {
   if (error) {
