bcdb4380aed060113b48a46e9dd6badc3a498197
Pedro <pedro@webglsoft.com>
2023-02-07 15:20:03 +0800

    :sparkles: working version

--- postinstall.cjs
+++ postinstall.cjs
@@ -3,20 +3,33 @@ const exec = require('child_process').exec;
 const fs = require('fs');
 const path = require('path');
 
-const templatePath = __dirname;
+const templatePath = process.cwd();
 const targetPath = path.resolve(process.cwd(), process.argv[2]);
 
 fs.mkdirSync(targetPath, { recursive: true });
 
 const copyFile = (file) => {
+  if (file === path.basename(targetPath)) {
+    return;
+  }
+
   const templateFile = path.resolve(templatePath, file);
   const targetFile = path.resolve(targetPath, file);
 
-  const content = fs.readFileSync(templateFile, 'utf-8');
-  fs.writeFileSync(targetFile, content);
+  if (fs.lstatSync(templateFile).isDirectory()) {
+    fs.mkdirSync(targetFile, { recursive: true });
+    fs.readdirSync(templateFile).forEach((subFile) => {
+      copyFile(path.join(file, subFile));
+    });
+  } else {
+    const content = fs.readFileSync(templateFile, 'utf-8');
+    fs.writeFileSync(targetFile, content);
+  }
 };
 
-fs.readdirSync(templatePath).forEach(copyFile);
+fs.readdirSync(templatePath)
+  .filter((file) => file !== 'template.json' && file !== 'postinstall.cjs')
+  .forEach(copyFile);
 
 exec('npm install', (error, stdout, stderr) => {
   if (error) {
